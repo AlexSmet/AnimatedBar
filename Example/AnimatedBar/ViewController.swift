3d8d8f62eb2743bddf0b086b872b33e2dd8b9c76
Alex Smetannikov <alexsmetdev@gmail.com>
2019-04-06 14:02:51 +0300

    Example added. Tests are removed from the Example project.

--- Example/AnimatedBar/ViewController.swift
+++ Example/AnimatedBar/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 import AnimatedBar
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstAnimatedBar: AnimatedBar!
+    @IBOutlet weak var secondAnimatedBar: AnimatedBar!
+    @IBOutlet weak var thirdAnimatedBar: AnimatedBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        configureAnimatedBars()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        animateBars()
     }
 
+    func configureAnimatedBars() {
+        firstAnimatedBar.backgroundColor = .clear
+        firstAnimatedBar.barColors = [UIColor(red: 67/255, green: 198/255, blue: 172/255, alpha: 1),
+                                      UIColor(red: 248/255, green: 255/255, blue: 174/255, alpha: 1)]
+
+        secondAnimatedBar.backgroundColor = .clear
+        secondAnimatedBar.barColors = [UIColor(red: 252/255, green: 92/255, blue: 125/255, alpha: 1),
+                                       UIColor(red: 147/255, green: 84/255, blue: 227/255, alpha: 1),
+                                       UIColor(red: 106/255, green: 130/255, blue: 251/255, alpha: 1)]
+
+        thirdAnimatedBar.backgroundColor = .clear
+        thirdAnimatedBar.barColors = [UIColor(red: 0/255, green: 12/255, blue: 64/255, alpha: 1),
+                                      .white]
+        thirdAnimatedBar.borderColor = .lightGray
+        thirdAnimatedBar.borderWidth = 0.5
+    }
+
+    func animateBars() {
+        thirdAnimatedBar.setPosition(0, withDuration: 0)  // Just cleaning bar
+
+        firstAnimatedBar.setPosition(0.2, withDuration: 1.4)
+        secondAnimatedBar.setPosition(0.9, withDuration: 1.4) { _ in
+            self.thirdAnimatedBar.setPosition(0.9, withDuration: 1.4)
+        }
+    }
+
+    @IBAction func onPushSartButton(_ sender: UIButton) {
+        animateBars()
+    }
 }
 
